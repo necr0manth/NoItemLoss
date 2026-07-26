e131df71e92de6a4a44fd9ab692dc5e9ed0c9848
Necr0manth <tamerik.troll@mail.ru>
2026-07-26 23:55:13 +0300

    chore: log mod initialization

--- common/src/main/kotlin/dev/necr0manthre/noitemloss/NoItemLoss.kt
+++ common/src/main/kotlin/dev/necr0manthre/noitemloss/NoItemLoss.kt
@@ -2,5 +2,7 @@ package dev.necr0manthre.noitemloss
 
 object NoItemLoss {
     const val MOD_ID = "noitemloss"
-    fun init() {}
+    fun init() {
+        println("NoItemLoss Initialized!")
+    }
 }
